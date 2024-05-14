9af93bcaa2d366ac2e71c048cf2010d4769a974e
João Eudes Lima <joaoeudes7@gmail.com>
2024-05-14 16:17:04 -0300

    bump version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,7 @@ publishing {
             from(components.findByName("release"))
             groupId = "com.github.joaoeudes7"
             artifactId = "paginationfast"
-            version = "1.0.0"
+            version = "1.0.1"
         }
     }
 
